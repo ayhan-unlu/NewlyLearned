42266176c6d400e12a57f89e57c957dc3cb6ba17
Ayhan Unlu <103220953+ayhan-unlu@users.noreply.github.com>
2025-09-12 11:06:53 +0300

    Design Patterns (Mosh source added)

--- src/_2025_09_06_DesignPattern/_00_Source.java
+++ src/_2025_09_06_DesignPattern/_00_Source.java
@@ -1,5 +1,6 @@
 package _2025_09_06_DesignPattern;
 
 public class _00_Source {
+    String source=_2025_09_05__Sources.mosh;
 
 }
---
+++ src/_2025_09_06_DesignPattern/_2025_09_05__Sources.java
@@ -0,0 +1,6 @@
+package _2025_09_06_DesignPattern;
+
+public class _2025_09_05__Sources {
+    public static String broCode ="https://www.youtube.com/@BroCodez";
+    public static String mosh = "https://www.youtube.com/@programmingwithmosh";
+}
